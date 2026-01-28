5e091ab58ed05fe64e07a1cbdea0771be2d39c47
bangalan01 <paridakhusi01@gmail.com>
2026-01-29 00:13:38 +0530

    Initial commit

---
+++ src/Counter.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [count, setCount] = useState(0)
+
+    function handleIncreament(){
+        console.log("Click")
+        setCount(count+1)
+    }
+    const handleDecreament =()=>{
+        setCount(count-1)
+    }
+    let handleReset=()=>{
+        setCount(0)
+    }
+
+  return (
+    <>
+    <h1>Counter Application</h1>
+    <h3> Count : {count}</h3>
+    <button onClick={()=>handleIncreament()}>Increament</button>
+    <button onClick={()=>handleDecreament()}>Decreament</button>
+    <button onClick={()=>handleReset()}>Reset</button>
+    </>
+  )
+}
+
+export default Counter
